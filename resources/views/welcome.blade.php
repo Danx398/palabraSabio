06e7e21b8a755d5467733bd760ae5290b8701cc0
Daniel <danidannniel398@gmail.com>
2022-09-20 12:34:46 -0500

    v1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/main')
+@section('contenido')
+    <div class="container">
+        <div class="row">
+            <div class="col mt-3">
+                <h1>Hello world</h1>
+            </div>
+            
+        </div>
+    </div>
+@endsection
